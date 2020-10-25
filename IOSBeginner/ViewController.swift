e237735de4d9faaca60521f9a59fbeaf4d993edd
BaoDang <hoaibaodang1997@gmail.com>
2020-10-25 21:22:07 +0700

    setup project

---
+++ IOSBeginner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSBeginner
+//
+//  Created by Macbook Pro 13 on 10/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
